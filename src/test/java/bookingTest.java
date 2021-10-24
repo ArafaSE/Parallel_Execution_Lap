5c96541a9078fca3c9215162314f1045272f629e
ArafaSE <mohamed.arafa@bosta.co>
2021-10-24 17:25:07 +0200

    First commit

---
+++ src/test/java/bookingTest.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class bookingTest {
+
+    WebDriver driver;
+    String ChromePath = System.getProperty("user.dir") + "/drivers/chromedriver.exe";
+
+    @BeforeTest
+    @Parameters({"remoteUrl"})
+    public void beforeTest(String remoteURL) throws MalformedURLException {
+        ChromeOptions chromeOptions= new ChromeOptions();
+        System.setProperty("webdriver.chrome.driver", ChromePath);
+        driver = new RemoteWebDriver(new URL(remoteURL), chromeOptions);
+    }
+
+    @Test
+    public void bookingTest1 () throws InterruptedException {
+
+          driver.navigate().to("https://www.booking.com/");
+          driver.manage().window().maximize();
+          System.out.println("Booking test Running with ID: " + Thread.currentThread().getId());
+          Thread.sleep(5000);
+    }
+
+    @AfterSuite
+    public void tearDown(){
+            driver.close();
+    }
+}
---
+++ src/test/java/osnTest.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class osnTest {
+
+    WebDriver driver;
+    String ChromePath = System.getProperty("user.dir") + "/drivers/chromedriver.exe";
+
+    @BeforeTest
+    @Parameters({"remoteUrl"})
+    public void beforeTest(String remoteURL) throws MalformedURLException {
+        ChromeOptions chromeOptions= new ChromeOptions();
+        System.setProperty("webdriver.chrome.driver", ChromePath);
+        driver = new RemoteWebDriver(new URL(remoteURL), chromeOptions);
+    }
+    @Test
+    public void osnTest1() throws InterruptedException {
+
+        driver.get("https://stream.osn.com/en-eg/home");
+        driver.manage().window().maximize();
+        System.out.println("Booking test Running with ID: " + Thread.currentThread().getId());
+        Thread.sleep(5000);
+    }
+    @AfterSuite
+    public void tearDown(){
+        driver.close();
+    }
+
+}
